09f40327f2c316ce67dd4054d4fc6b7a592ce44a
einarorn <einarorn79@gmail.com>
2022-04-15 12:46:00 +0000

    add tests for points

---
+++ internal/domain/points/points.go
@@ -0,0 +1,13 @@
+package points
+
+type Points struct {
+	 value int	
+}
+
+func (s *Points) GetPoints() int {
+	return -1
+}
+
+func (s *Points) AddPoints(value int) {
+	
+}
---
+++ internal/domain/points/points_test.go
@@ -0,0 +1,43 @@
+package points_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"go-bricks/internal/domain/points"
+)
+
+func TestPoints_GetPoints(t *testing.T) {
+	t.Run("Inital points value should be zero", func(t *testing.T) {
+		p := points.Points{}
+		expected := 0
+
+		actual := p.GetPoints()
+
+		assert.Equal(t, expected, actual, "points value is not zero")
+	})
+}
+
+func TestPoints_AddPoints(t *testing.T) {
+	t.Run("Add 50 points single time", func(t *testing.T) {
+		p := points.Points{}
+		expected := 50
+		p.AddPoints(50)
+
+		actual := p.GetPoints()
+
+		assert.Equal(t, expected, actual, "points value is not 50")
+	})
+
+	t.Run("Add 50 points two times", func(t *testing.T) {
+		p := points.Points{}
+		expected := 100
+		p.AddPoints(50)
+		p.AddPoints(50)
+
+		actual := p.GetPoints()
+
+		assert.Equal(t, expected, actual, "points value is not 100")
+	})
+}
